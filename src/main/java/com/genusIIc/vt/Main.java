bfa5ffd04b4ce3bf463b2ee3f4b2c5e317a89569
Vent-Tardar <79703422+Vent-Tardar@users.noreply.github.com>
2021-04-22 16:20:24 +0300

    Add files via upload

--- src/main/java/com/genusIIc/vt/Main.java
+++ src/main/java/com/genusIIc/vt/Main.java
@@ -1,9 +1,13 @@
 package com.genusIIc.vt;
 
+import org.apache.logging.log4j.*;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
-        if ((args.length == 1) || (args.length == 0)){
+        if ((args.length > 2) || (args.length < 2)){
             System.err.println("You didn't enter the parameters completely. Check them out.");
+            logger.error("Parameters entered incorrectly");
         } else {
             ComparisonDoc cd = new ComparisonDoc();
             cd.compare(args[0], args[1]);
